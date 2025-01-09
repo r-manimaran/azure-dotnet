1dbd7981e7bc046c734d74ed999436985325ce55
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-01-08 22:37:03 -0500

    Testing CICD

--- AzFuncDevOps/CICDFunc/Greetings.cs
+++ AzFuncDevOps/CICDFunc/Greetings.cs
@@ -18,7 +18,7 @@ namespace CICDFunc
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions - CI1!");
+            return new OkObjectResult("Welcome to Azure Functions - CI2!");
         }
     }
 }
