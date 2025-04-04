ea46fcdb70c5b3cd6135c965adab2471006c3f3e
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-04-04 14:58:42 -0400

    Added Docker-compose, Serilog config to FileProcessor

---
+++ AzServicesApp/FileProcessor/FileProcessorJob.cs
@@ -0,0 +1,20 @@
+﻿
+namespace FileProcessor;
+
+public class FileProcessorJob : BackgroundService
+{
+    private readonly ILogger<FileProcessorJob> _logging;
+
+    public FileProcessorJob(ILogger<FileProcessorJob> logging)
+    {
+        _logging = logging;
+    }
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+       
+        while(!stoppingToken.IsCancellationRequested)
+        {
+
+        }
+    }
+}
--- AzServicesApp/FileProcessor/Program.cs
+++ AzServicesApp/FileProcessor/Program.cs
@@ -1,4 +1,17 @@
+using FileProcessor;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddHeaderPropagation(opt => opt.Headers.Add("azure-correlation-id"));
+
+builder.Services.AddHostedService<FileProcessorJob>();
+
+builder.Host.UseSerilog((context, configuration) => configuration
+            .ReadFrom.Configuration(context.Configuration));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
